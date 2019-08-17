a614e85228fd263db105d301b6edc69752d6dd62
Souvikcse <sayanwinz@gmail.com>
2019-08-18 02:37:40 +0530

    changed

--- Intro/async_advanced.js
+++ Intro/async_advanced.js
@@ -1,11 +1,16 @@
-let a=false;
-
-setTimeout(function () {
-    a=true;
-}, 2000);
-
-while (!a){
-    console.log(1);
+function some_async_task(callback) {
+    console.log("Beginning of task");
+    setTimeout(function () {
+        console.log("End of task");
+        callback();
+    }, 2000);
 }
+let some_promise=function () {
+    return new Promise(function (resolve, reject) {
+        some_async_task(resolve);
+    });
+}
+some_promise();
+
 
 
