5a987638b33e1aea8773aaffcd1de88e3eb5931e
Souvikcse <sayanwinz@gmail.com>
2019-08-19 15:09:00 +0530

    changed
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- file_systm/read.js
+++ file_systm/read.js
@@ -3,5 +3,17 @@ const fs = require('fs');
 fs.readFile(__dirname + '/file1.txt', function (err, data) {
     if (err) throw err;
 
-    console.log(data.toString());
+    let ar= data.toString().split('\n');
+    let animals = new Map();
+    for (let i=0;i<ar.length;i++){
+        if (!animals.has(ar[i]) ){
+            animals.set(ar[i], 1);
+        }
+        else
+            animals.set(ar[i], animals.get(ar[i]) + 1);
+    }
+    for (let [key, value] of animals){
+        if (key!=='')
+        console.log(key + ' => '+ value);
+    }
 })
